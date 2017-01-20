ed8ee10e0bacc934cc76f020f9d412ecb2319c36
Filiberto Santillan <fili06604@gmail.com>
2017-01-20 13:47:24 -0600

    ✨ Support for emojis in posts

--- src/js-dev/fili/default.js
+++ src/js-dev/fili/default.js
@@ -5,3 +5,19 @@ $('#open-menu').click(function() {
 $('#close-menu').click(function() {
   $('#menu').removeClass('menu-active');
 });
+
+var div = document.createElement('div');
+div.textContent = 'I \u2764\uFE0F emoji!';
+document.body.appendChild(div);
+
+twemoji.parse(document.body);
+
+var img = div.querySelector('img');
+
+// note the div is preserved
+img.parentNode === div; // true
+
+img.src;        // https://twemoji.maxcdn.com/36x36/2764.png
+img.alt;        // \u2764\uFE0F
+img.className;  // emoji
+img.draggable;  // false
