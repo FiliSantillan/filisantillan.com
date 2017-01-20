9912fa60e05439c5a3072089f9c800948840c1cc
Filiberto Santillan <fili06604@gmail.com>
2017-01-20 15:13:31 -0600

    🐛 Fixing bugs in disqus

--- src/js-dev/fili/default.js
+++ src/js-dev/fili/default.js
@@ -8,15 +8,5 @@ $('#close-menu').click(function() {
 
 var div = document.createElement('div');
 document.body.appendChild(div);
-
 twemoji.parse(document.body);
-
 var img = div.querySelector('img');
-
-// note the div is preserved
-img.parentNode === div; // true
-
-img.src;        // https://twemoji.maxcdn.com/36x36/2764.png
-img.alt;        // \u2764\uFE0F
-img.className;  // emoji
-img.draggable;  // false
--- src/js-dev/scripts.js
+++ src/js-dev/scripts.js
@@ -8,18 +8,9 @@ $('#close-menu').click(function() {
 
 var div = document.createElement('div');
 document.body.appendChild(div);
-
 twemoji.parse(document.body);
-
 var img = div.querySelector('img');
 
-// note the div is preserved
-img.parentNode === div; // true
-
-img.src;        // https://twemoji.maxcdn.com/36x36/2764.png
-img.alt;        // \u2764\uFE0F
-img.className;  // emoji
-img.draggable;  // false
 disqus = function (newIdentifier, newUrl, newTitle) {
     if ($('#disqus_thread').length) {
         if (typeof DISQUS === 'undefined') {
