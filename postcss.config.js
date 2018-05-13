8f8bdfd2f365d26b41e902c24c6fcd69d0c08dc5
Fili Santillan <fili06604@gmail.com>
2018-05-13 12:15:35 -0500

    🔧 Settings: Add sizes fonts

--- postcss.config.js
+++ postcss.config.js
@@ -5,11 +5,24 @@ module.exports = {
             features: {
                 autoprefixer: {
                     flexbox: false,
+                    grid: false
                 },
                 customProperties: false
             }
         }),
         require("postcss-font-magician"),
-        require("css-mqpacker")
+        require("css-mqpacker")({
+            variants: {
+                Montserrat: {
+                    300: [],
+                    700: []
+                },
+                Nunito: {
+                    300: [],
+                    400: [],
+                    700: []
+                }
+            }
+        })
     ]
 };
