7e28b9d33b39d64282ed91e0b2f6cdb10a562e2e
Fili Santillan <fili06604@gmail.com>
2018-05-13 20:05:28 -0500

    💄 🚧 Articles in progress

--- src/js/home.js
+++ src/js/home.js
@@ -7,7 +7,10 @@ import "../styles/base/base.css";
 import "../styles/utilities/u-typography.css";
 import "../styles/utilities/u-elements.css";
 
+import "../styles/objects/buttons.css";
+
 import "../styles/components/header/header.css";
 import "../styles/components/cover/cover.css";
+import "../styles/components/posts/article.css";
 
 console.log("Hello");
