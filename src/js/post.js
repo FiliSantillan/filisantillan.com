e36a2f6496d51f271bdd14aa8451d89c11bdf70c
Fili Santillan <fili06604@gmail.com>
2018-05-26 05:42:05 -0500

    🚧 💄 Cover post: ready (mobile)

---
+++ src/js/post.js
@@ -0,0 +1,13 @@
+import "../styles/vendor/normalize.css";
+
+import "../styles/variables/variables.css";
+
+import "../styles/base/base.css";
+
+import "../styles/utilities/u-typography.css";
+import "../styles/utilities/u-elements.css";
+
+import "../styles/objects/buttons.css";
+
+import "../styles/components/header/header.css";
+import "../styles/components/cover/cover-post.css";
