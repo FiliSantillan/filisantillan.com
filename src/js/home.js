7ddc3685887d128237474aec3dc96dc8e2397ff6
Fili Santillan <fili06604@gmail.com>
2018-05-26 18:32:37 -0500

    ✨ 💄 Icons added

--- src/js/home.js
+++ src/js/home.js
@@ -8,6 +8,7 @@ import "../styles/utilities/u-typography.css";
 import "../styles/utilities/u-elements.css";
 
 import "../styles/objects/buttons.css";
+import "../styles/objects/icons.css";
 
 import "../styles/components/header/header.css";
 import "../styles/components/cover/cover.css";
