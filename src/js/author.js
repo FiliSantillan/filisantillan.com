1fb8bb317828713fe636938df79b5565375796e0
Fili Santillan <fili06604@gmail.com>
2018-07-06 00:29:00 -0500

    🎨 Change in the structure of styles
    Now the imports are with CSS (@import) so that there is compatibility with mqpacker

--- src/js/author.js
+++ src/js/author.js
@@ -1,22 +1,11 @@
 import "../styles/vendor/normalize.css";
 
-import "../styles/variables/variables.css";
-
-import "../styles/base/base.css";
-
-import "../styles/utilities/u-typography.css";
-import "../styles/utilities/u-elements.css";
 import "../styles/utilities/u-icons.css";
 
-import "../styles/objects/buttons.css";
 import "../styles/objects/icons.css";
 import "../styles/objects/icon-tags.css";
 
-import "../styles/components/header/header.css";
-import "../styles/components/header/navigation.css";
-import "../styles/components/cover/cover.css";
-import "../styles/components/posts/article.css";
-import "../styles/components/footer/footer.css";
+import "../styles/main-author.css";
 
 /* Javascript */
 
--- src/js/post.js
+++ src/js/post.js
@@ -1,23 +1,12 @@
 import "../styles/vendor/normalize.css";
 import "../styles/vendor/prism.css";
 
-import "../styles/variables/variables.css";
-
-import "../styles/base/base.css";
-
-import "../styles/utilities/u-typography.css";
-import "../styles/utilities/u-elements.css";
 import "../styles/utilities/u-icons.css";
 
-import "../styles/objects/buttons.css";
 import "../styles/objects/icons.css";
 import "../styles/objects/icon-tags.css";
 
-import "../styles/components/header/header.css";
-import "../styles/components/header/navigation.css";
-import "../styles/components/cover/cover-post.css";
-import "../styles/components/posts/post.css";
-import "../styles/components/footer/footer.css";
+import "../styles/main-post.css";
 
 import "./vendor/prism";
 
