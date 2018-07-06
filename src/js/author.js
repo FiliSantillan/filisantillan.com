96827db83e7b4be2e79b54bb38961e945f20ec78
Fili Santillan <fili06604@gmail.com>
2018-07-06 01:26:44 -0500

    🔥 Remove icons (tags) to css

--- src/js/author.js
+++ src/js/author.js
@@ -3,7 +3,6 @@ import "../styles/vendor/normalize.css";
 import "../styles/utilities/u-icons.css";
 
 import "../styles/objects/icons.css";
-import "../styles/objects/icon-tags.css";
 
 import "../styles/main-author.css";
 
