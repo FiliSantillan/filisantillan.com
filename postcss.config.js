65af9ffc3d8c52a40be31a50008d4b0883af8e17
Fili Santillan <fili06604@gmail.com>
2018-07-07 16:23:14 -0500

    🔥 ➖ Unnecessary dependencies removed

--- postcss.config.js
+++ postcss.config.js
@@ -30,9 +30,6 @@ module.exports = {
         }),
         require("css-mqpacker")({
             sort: true
-        }),
-        // require("cssnano")({
-        //     autoprefixer: false,
-        // })
+        })
     ]
 };
