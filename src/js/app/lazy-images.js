a5c8568e184f1435c3a04221e31325a56e690394
Fili Santillan <fili06604@gmail.com>
2018-08-04 23:52:02 -0500

    ⚡️ Improving performance: Images now load with scroll (Pages)

---
+++ src/js/app/lazy-images.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function() {
+  let lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
+
+  let lazyImageObserver = new IntersectionObserver(function(entries, observer) {
+    entries.forEach(function(entry) {
+      if (entry.isIntersecting) {
+        let lazyImage = entry.target;
+        lazyImage.src = lazyImage.dataset.src;
+        lazyImage.classList.remove("lazy");
+        lazyImageObserver.unobserve(lazyImage);
+      }
+    });
+  });
+
+  lazyImages.forEach(function(lazyImage) {
+    lazyImageObserver.observe(lazyImage);
+  });
+});
--- src/js/home.js
+++ src/js/home.js
@@ -7,19 +7,20 @@ import "../styles/main-home.css";
 /* Javascript */
 
 import { drawImage } from "./app/drawImage";
+import lazyImages from "./app/lazy-images";
 
 import imageMacbook from "../images/macbook.png";
 import imageMacbook2x from "../images/macbook@2x.png";
 import imageMacbook3x from "../images/macbook@3x.png";
 
 const imageModule = {
-    figureClass: "cover__image-wrapper",
-    imgClass: "cover__image",
-    alt: "Macbook Code",
-    src: imageMacbook,
-    srcset2x: imageMacbook2x,
-    srcset3x: imageMacbook3x
-}
+  figureClass: "cover__image-wrapper",
+  imgClass: "cover__image",
+  alt: "Macbook Code",
+  src: imageMacbook,
+  srcset2x: imageMacbook2x,
+  srcset3x: imageMacbook3x
+};
 
 drawImage("cover-home", imageModule);
 
