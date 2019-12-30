1f4ae915fe8f66a06c3d6978e191c1089401ce5e
Fili Santillán <fili06604@gmail.com>
2019-12-30 03:37:09 -0600

    🚧 Working and testing in Home

--- src/js/home.js
+++ src/js/home.js
@@ -9,3 +9,27 @@ import "../styles/main-home.css";
 // import lazyImages from "./app/lazy-images";
 
 import "./app/navigation";
+
+const $featurePost = document.getElementById("feature-post");
+
+const docElement = document.documentElement;
+const spacerSize = getComputedStyle(docElement).getPropertyValue(
+  "--spacer-size"
+);
+
+const spacerFeaturePost =
+  $featurePost.offsetHeight - parseFloat(spacerSize) * 16 * 4;
+
+docElement.style.setProperty("--spacer-feature-post", `${spacerFeaturePost}px`);
+
+function reportWindowSize() {
+  const spacerFeaturePost =
+    $featurePost.offsetHeight - parseFloat(spacerSize) * 16 * 4;
+
+  docElement.style.setProperty(
+    "--spacer-feature-post",
+    `${spacerFeaturePost}px`
+  );
+}
+
+window.onresize = reportWindowSize;
