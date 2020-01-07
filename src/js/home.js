4b677be170260482510906a795ed681b49079225
Fili Santillán <fili06604@gmail.com>
2020-01-07 00:05:39 -0600

    💄 ✨ Change UI icons and test new ui site

--- src/js/home.js
+++ src/js/home.js
@@ -4,11 +4,28 @@ import "../styles/utilities/u-icons.css";
 
 import "../styles/main-home.css";
 
+// Hacer función para que tome los datos de data-icon: icon-star y ese sea el que tome.
+
+import searchIcon from "../../src/images/icons/search.svg";
+import menuIcon from "../../src/images/icons/menu.svg"
+import starIcon from "../../src/images/icons/star-filled.svg";
+
+const searchImg = document.querySelector("#icon-search");
+const settingsImg = document.querySelector("#icon-settings");
+const menuImg = document.querySelector("#icon-menu");
+const starImg = document.querySelector("#icon-star");
+
+searchImg.src = searchIcon;
+settingsImg.src = menuIcon;
+menuImg.src = menuIcon;
+starImg.src = starIcon;
+
 /* Javascript */
 
 // import lazyImages from "./app/lazy-images";
 
 import "./app/navigation";
+
 
 const $featurePost = document.getElementById("feature-post");
 
