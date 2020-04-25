04141b285ac9ecbf8c4fcf6eb9d08af195ac8cbe
Fili Santillán <fili06604@gmail.com>
2020-04-25 15:14:06 -0500

    🚧 💩 💄 Post UI in progress ...

--- src/pages/Post/Post.js
+++ src/pages/Post/Post.js
@@ -5,3 +5,29 @@ import "./Post.css";
 import "../../vendor/prism";
 
 /* Javascript */
+
+document.addEventListener("DOMContentLoaded", () => {
+    const codeElementList = document.querySelectorAll("pre[class*='language']");
+
+    for (let codeElement of codeElementList) {
+        const languageText = codeElement.className
+            .replace("language-", "")
+            .trim();
+
+        const brotherElement = codeElement.previousSibling,
+            divContainer = document.createElement("div"),
+            divLanguage = document.createElement("div"),
+            strongLanguage = document.createElement("strong");
+
+        strongLanguage.textContent = languageText;
+
+        divContainer.classList.add("code-container");
+        divLanguage.classList.add("code-container__language");
+        strongLanguage.classList.add("lcode-container__text");
+
+        divLanguage.appendChild(strongLanguage);
+        divContainer.appendChild(divLanguage);
+        divContainer.appendChild(codeElement);
+        brotherElement.insertAdjacentElement("afterend", divContainer);
+    }
+});
