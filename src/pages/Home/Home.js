369a5c3a60dfcafed67e8bfdc4bf0340cfd6a596
Fili Santillán <fili06604@gmail.com>
2020-04-28 16:52:58 -0500

    🚧 ✨ Search feature in progress

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,4 +1,13 @@
 import "../../vendor/normalize.css";
 import "./Home.css";
 
+import "ghost-finder";
+
 /* Javascript */
+
+new GhostFinder({
+    input: "#search-input",
+    showResult: "#search-result",
+    resultTemplate: "<ul class='search-results-wrapper'>##results</ul>",
+    contentApiKey: "e08066d77dbdb6606d68c93656",
+});
