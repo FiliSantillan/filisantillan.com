3f1b5356cc75c143c3f0890584e805ffbd211968
Fili Santillán <fili06604@gmail.com>
2020-04-30 22:50:24 -0500

    💄 💩 Subscription form states completed

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -5,6 +5,40 @@ import "ghost-finder";
 
 /* Javascript */
 
+const target = document.getElementById("subcription-form");
+
+let observer = new MutationObserver(mutations => {
+    let mutation = mutations[0];
+
+    if (mutation.attributeName === "class") {
+        const state = mutation.target.classList[1];
+        const button = document.getElementById("subscription-button");
+        let pElement = document.createElement("p");
+        pElement.classList.add("subscription__sucess");
+        pElement.textContent =
+            "Tu subscripción esta casi completa, por favor revisa tu correo para finalizar.";
+
+        if (state === "loading") {
+            button.textContent = "Cargando";
+        } else if (state === "success") {
+            button.textContent = "Ser parte";
+            button.setAttribute("disabled", "");
+            target.appendChild(pElement);
+            observer.disconnect();
+        } else {
+            button.textContent = "Ser parte";
+        }
+    }
+});
+
+let config = {
+    attributes: true,
+    childList: false,
+    characterData: false,
+};
+
+observer.observe(target, config);
+
 const iconSearch = document.getElementById("icon-search");
 const iconClose = document.getElementById("icon-close");
 const searchContainer = document.getElementById("search-container");
