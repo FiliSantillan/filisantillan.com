9443f87e0e8a290ac10eeefcbeb9bb635b0df3a9
Fili Santillán <fili06604@gmail.com>
2020-05-02 08:16:35 -0500

    ✨ 💄 Support for images added depending on the device

--- postcss.config.js
+++ postcss.config.js
@@ -1,35 +1,37 @@
 module.exports = {
-  plugins: [
-    require("postcss-import"),
-    require("postcss-preset-env")({
-      stage: 0,
-      features: {
-        autoprefixer: {
-          flexbox: false,
-          grid: false,
-        },
-        "custom-properties": false,
-        rem: false,
-      },
-    }),
-    require("postcss-font-magician")({
-      display: "swap",
-      variants: {
-        Montserrat: {
-          300: [],
-          400: [],
-          500: [],
-          600: [],
-          700: [],
-        },
-        "Source Code Pro": {
-          400: [],
-          700: [],
-        },
-      },
-    }),
-    require("css-mqpacker")({
-      sort: true,
-    }),
-  ],
+    plugins: [
+        require("postcss-import"),
+        require("postcss-preset-env")({
+            stage: 0,
+            browsers: "last 2 versions",
+            features: {
+                autoprefixer: {
+                    flexbox: false,
+                    grid: false,
+                    imageSet: true,
+                },
+                "custom-properties": false,
+                rem: false,
+            },
+        }),
+        require("postcss-font-magician")({
+            display: "swap",
+            variants: {
+                Montserrat: {
+                    300: [],
+                    400: [],
+                    500: [],
+                    600: [],
+                    700: [],
+                },
+                "Source Code Pro": {
+                    400: [],
+                    700: [],
+                },
+            },
+        }),
+        require("css-mqpacker")({
+            sort: true,
+        }),
+    ],
 };
