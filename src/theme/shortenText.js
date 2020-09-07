95d220514948e3a399ac258086b5656bc94b1e6f
Fili Santillán <fili06604@gmail.com>
2020-09-07 00:28:07 -0500

    🎨 remove console test

--- src/theme/shortenText.js
+++ src/theme/shortenText.js
@@ -1,10 +1,6 @@
 export const shortenText = (textElement, limit) => {
     const onlyText = textElement.textContent.trim();
 
-    console.log(onlyText);
-    console.log(onlyText.length);
-    console.log(onlyText.substring(0, 10));
-
     if (onlyText.length > limit) {
         textElement.textContent = `${onlyText.substring(0, limit)}...`;
     }
